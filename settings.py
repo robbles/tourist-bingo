6a9b9916663440ab086e09d15101d1b39d0d7498
robbles <odwyerrob@gmail.com>
2014-04-15 20:48:24 -0700

    setting for redis password

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ from os import environ as env
 REDIS_HOST = env.get('REDIS_HOST', 'localhost')
 REDIS_PORT = int(env.get('REDIS_PORT', 6379))
 REDIS_DB = int(env.get('REDIS_DB', 10))
+REDIS_PASS = env.get('REDIS_PASS', None)
 
 PASSWORD = env.get('PASSWORD', 'password')
 SECRET_KEY = env.get('SECRET_KEY', 'UeYYonX1w9jSGAaCwu3hy9tp2le1dSxrNHodn0HXpH3CzvxZCoBvnJUgNZI9mGJi')
